d3bbcf48d6e3e7bfa395261778f40161962ff7de
mdsomerfield <mark.somerfield@makersacademy.com>
2018-04-04 18:10:18 +0100

    Possible to submit scores through the interface

--- src/interface.js
+++ src/interface.js
@@ -5,9 +5,15 @@ function initialise() {
   var frameElement = frameView.render();
   $('#scorecard-container').append(frameElement);
 
-  var controlsView = new ControlsView();
+  var controlsView = new ControlsView(addScore);
   var controlsElement = controlsView.render();
   $('#controls-container').append(controlsElement);
+
+  function addScore(value) {
+    frame.roll(parseInt(value));
+    frameView.update();
+  }
 }
+
 
 $(document).ready(initialise);
